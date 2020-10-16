92bf9232123d5fe3723893e267685a53ccc46672
aidabyte <ablinstrubyte@gmail.com>
2020-10-15 19:58:40 -0500

    updated files

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-const generateMarkdown= require("./utilis/generateMarkdown")
+var inquirer = require('inquirer');
+
+var fs = require('fs');
+
+const generateMarkdown= require("./utils/generateMarkdown");
+
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please enter your project description",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please enter your installation instructions",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Please enter your usage information",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Please enter your contribution guidelines",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Please enter your test instructions",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "choose a license",
+        name: "license",
+        choices: ["MIT","Apache License 2.0","Mozilla Public License 2.0"]
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+   
+
+    
 
 ];
 
@@ -9,13 +63,30 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
-    generateMarkdown(data);
+    //usign built in package for fs writefile
+    fs.writeFile(fileName, generateMarkdown(data), function(error) {
+        if (error) 
+        return error
+        
+    })
 }
 
 // function to initialize program
 function init() {
+    //using .prompt to pass my questions to user
+    inquirer.prompt(questions )
+    //using .then method to promise users feedback in answers 
+    .then(function(answers) {
+    let readMeInfo = generateMarkdown(answers);
+    //writing the answers into the readme.txt after the user has completed their answers
+        writeToFile("Readme.txt", answers);
+    })
+        
 
+   
 }
+
+
 
 // function call to initialize program
 init();
